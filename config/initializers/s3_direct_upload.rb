0aa720dc6f68bf0e38d26c9ae467ce173187dffc
Sheharyar Naseer <sheharyar92@live.com>
2013-12-05 14:41:51 +0500

    Small Fixes

--- config/initializers/s3_direct_upload.rb
+++ config/initializers/s3_direct_upload.rb
@@ -1,7 +1,7 @@
 S3DirectUpload.config do |c|
-	c.access_key_id		= ENV['AWS_ACCESS_KEY_ID']
+	c.access_key_id		  = ENV['AWS_ACCESS_KEY_ID']
 	c.secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-	c.bucket 			= ENV['AWS_S3_BUCKET']
+	c.bucket 			      = ENV['AWS_S3_BUCKET']
 end
 
 ## URL MonkeyPatch
